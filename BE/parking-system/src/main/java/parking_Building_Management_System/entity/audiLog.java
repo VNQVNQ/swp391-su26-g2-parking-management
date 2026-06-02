a32df8fc0d8872961561a15325b40b515d10b9d9
Nguyen Phuoc Sanh <npsanhz11@gmail.com>
2026-06-02 15:17:49 +0700

    add entity and dto parking_system

---
+++ BE/parking-system/src/main/java/parking_Building_Management_System/entity/audiLog.java
@@ -0,0 +1,26 @@
+package parking_Building_Management_System.entity;
+
+import com.team4.LaboratoryManagement.entity.User.User;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "audi_log")
+public class audiLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "audi_log_id")
+    private Long audiLogId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private String action;
+    private LocalDateTime createdAt;
+}
